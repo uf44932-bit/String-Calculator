5cb1c7403b59a83f3e0b9b3fce3cf089538bb4ac
uf44932-bit <uf44932@mail.harpercollege.edu>
2026-09-03 10:04:51 -0500

    Initial Commit

---
+++ String Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  String Calculator
+//
+//  Created by Anahi Munoz on 9/3/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
