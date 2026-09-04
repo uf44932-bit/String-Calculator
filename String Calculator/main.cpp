d3cb735ea75ec329743144570d6c26fb68b34140
uf44932-bit <uf44932@mail.harpercollege.edu>
2026-09-04 15:12:41 -0500

    first draft

--- String Calculator/main.cpp
+++ String Calculator/main.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <cassert>
 
 
@@ -14,8 +15,19 @@ int string_calculator(string str) {
     }
     
     int sum = 0;
-   
+    stringstream ss(str);
+    string number;
     
+    while (getline(ss, number, ',')){
+        stringstream sub_ss(number);
+        string ss_number;
+        
+        while (getline(sub_ss, ss_number, ';')) {
+            if(ss_number.empty()){
+                sum +=stoi(ss_number);
+            }
+        }
+    }
     
     
     return sum;
@@ -29,9 +41,11 @@ int string_calculator(string str) {
 int main()
 
 {
-  
+    assert(string_calculator(" ") == 0);
+    assert(string_calculator("1,2,3") == 6);
+    assert(string_calculator("1;2") == 3);
     
-    
+    cout << "everyting passed!";
     
     
     return EXIT_SUCCESS;
